164237d2649f2a2717528b13fdc4227a56374156
Nadya Chistyakova <npodtynchenko@list.ru>
2020-09-17 21:36:02 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 4500;
+        int amount = 20;
+        int bonus = price / amount;
+        System.out.println(bonus);
+    }
+}
